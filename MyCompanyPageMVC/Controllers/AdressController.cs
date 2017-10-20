20e1f119fa7bbcbf121894e46b783fcf8cf8ad33
Tomislav Stampfel <tomislav.stampfel@affecto.com>
2017-10-20 12:56:16 +0200

    Added my company

---
+++ MyCompanyPageMVC/Controllers/AdressController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCompanyPageMVC.Controllers
+{
+    public class AdressController : Controller
+    {
+        // GET: Adress
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Adress()
+        {
+            return View();
+        }
+    }
+}
